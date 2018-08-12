eaab25acb752acd66dabdbb3b5e9eb55957cffa7
mirekmts <mirekmts@gmail.com>
2018-08-12 20:17:03 +0200

    Make a fetch call for page

--- components/image.js
+++ components/image.js
@@ -1,5 +1,5 @@
 const Image = () => (
-    <img style={{width: '500px', display: 'block'}} src="https://images.pexels.com/photos/840326/pexels-photo-840326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="dog"/>
+    <img style={{display: 'block'}} src="https://images.pexels.com/photos/840326/pexels-photo-840326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=250" alt="dog"/>
 )
 
 export default Image
---
+++ pages/robots.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch'
+
+
+const Robots = (props) => {
+    return (
+        <div>
+            <h1>Robots</h1>
+            <Link href='/'>
+                <button>Home</button>
+            </Link>
+            <ul>
+                {
+                    props.robots.map(robot => (
+                        <li key={robot.id}>
+                        <Link href={`robots/${robot.id}`}>
+                            <a>{robot.name}</a>
+                        </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+Robots.getInitialProps = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+        robots: data,
+    }
+}
+
+export default Robots
